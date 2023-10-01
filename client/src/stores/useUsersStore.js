d8e7112f017c9736b78622b4a95b4f9bc9e3113b
Jake Wehder <jw@email.comjake>
2023-10-01 19:09:24 -0400

    creating logout and sign up actions for zustand user store

--- client/src/stores/useUsersStore.js
+++ client/src/stores/useUsersStore.js
@@ -1,5 +1,9 @@
 import { create } from 'zustand'
+import axios from 'axios'
 
+const getUser = axios.get("/me")
+
+const login = (user) => axios.post('/login', user)
 
 export const useStore = create((set) => ({
     user: null,
@@ -7,10 +11,21 @@ export const useStore = create((set) => ({
     isLoading: null,
     getUser: async () => {
         try {
-            set({ isLoading: true })
-            const 
+            set({ isLoading: true });
+            const response = await getUser();
+            set({ isLoading: false, data: response.data });
+        } catch(err) {
+            set({ error: err.message, isLoading: false });
+        }
+    },
+    login: async (user) => {
+        try {
+            set({ isLoading: true });
+            const response = await login(user);
+            set({ isLoading: false, data: response.data })
         } catch(err) {
             set({ error: err.message, isLoading: false })
         }
-    }
+    },
+    logout:
 }))
