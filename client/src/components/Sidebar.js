6a12acc77e46be2cc6558738e587479894db40ff
Paolo Alberca <91231062+omgitsmiles@users.noreply.github.com>
2023-10-17 15:40:15 -0400

    render user first and last name in nav bar when logged in

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -155,19 +155,14 @@ const MobileNav = ({ onOpen, selectedLink, ...rest }) => {
               <HStack>
                 <Avatar
                   size={'sm'}
-                  src={
-                    'https://images.unsplash.com/photo-1619946794135-5bc917a27793?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=b616b2c5b373a80ffc9636ba24f7a4a9'
-                  }
+                  bg='#2F4CED'
                 />
                 <VStack
                   display={{ base: 'none', md: 'flex' }}
                   alignItems="flex-start"
                   spacing="1px"
                   ml="2">
-                  <Text fontSize="sm">Justina Clark</Text>
-                  <Text fontSize="xs" color="gray.600">
-                    Admin
-                  </Text>
+                  <Text fontSize="sm">{user?.first_name + ' ' + user?.last_name}</Text>
                 </VStack>
                 <Box display={{ base: 'none', md: 'flex' }}>
                   <FiChevronDown />
